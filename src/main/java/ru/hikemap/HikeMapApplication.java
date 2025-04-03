8431835e51e31b43e5e002ee43b03c1d5f13ece3
ilya_groza <ilya.groza13@gmail.com>
2025-04-03 21:57:31 +0400

    feat: initialize Spring Boot application

---
+++ src/main/java/ru/hikemap/HikeMapApplication.java
@@ -0,0 +1,12 @@
+package ru.hikemap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HikeMapApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(HikeMapApplication.class, args);
+  }
+}
