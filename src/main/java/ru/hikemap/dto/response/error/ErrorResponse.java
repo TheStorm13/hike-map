9c5cf5dd8a71a5ce1960f6f03c20ef132d0169d4
ilya_groza <ilya.groza13@gmail.com>
2025-04-05 14:32:44 +0400

    feat: add error response dto

---
+++ src/main/java/ru/hikemap/dto/response/error/ErrorResponse.java
@@ -0,0 +1,14 @@
+package ru.hikemap.dto.response.error;
+
+import java.time.ZonedDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ErrorResponse {
+
+  private final int status;
+  private final String message;
+  private final ZonedDateTime timestamp;
+}
